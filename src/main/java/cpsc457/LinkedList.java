934e7a09545709a25c8889cf08cf737ac9da3774
Right2Drive <bjpfannm@ucalgary.ca>
2015-10-28 14:57:39 -0600

    Minor bug fixes

--- src/main/java/cpsc457/LinkedList.java
+++ src/main/java/cpsc457/LinkedList.java
@@ -7,18 +7,17 @@ import java.util.concurrent.*;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
-
 public class LinkedList<T> implements Iterable<T> {
-	
-	private int sizeM;
-	protected Node headM;
-	protected Node current;
+
+    private int sizeM;
+    protected Node<T> headM;
+    protected Node<T> current;
 
     public LinkedList() {
 
-	sizeM=0;
-	headM= null;
-	current=headM;
+        sizeM = 0;
+        headM = null;
+        current = headM;
     }
 
     public LinkedList<T> append(T t) {
@@ -30,39 +29,40 @@ public class LinkedList<T> implements Iterable<T> {
     }
 
     public boolean isEmpty() {
-        if(headM == null)
-		return true;
-	else
-		return false;
+        if (headM == null) {
+            return true;
+        } else {
+            return false;
+        }
     }
 
     public void clear() {
-	headM = null;
-	current = headM;
-	sizeM=0;
+        headM = null;
+        current = headM;
+        sizeM = 0;
     }
 
     public T get(int index) {
-	current = headM;
-	for(int i = 0; i<index; i++)
-		current = current.getNext();
-	return null;
-	//return current.getItem();
+        current = headM;
+        for (int i = 0; i < index; i++) {
+            current = current.getNext();
+        }
+        return current.getItem();
     }
 
     public void sort(Comparator<T> comp) {
-	new MergeSort<T>(comp).sort(this);
+        new MergeSort<T>(comp).sort(this);
     }
 
     public void par_sort(Comparator<T> comp) {
-	new MergeSort<T>(comp).parallel_sort(this);
+        new MergeSort<T>(comp).parallel_sort(this);
     }
 
     public static <T extends Comparable<T>> void par_sort(LinkedList<T> list) {
-	
+
     }
 
-    public static <T extends Comparable<T>> void sort(LinkedList<T> list){
+    public static <T extends Comparable<T>> void sort(LinkedList<T> list) {
         list.sort(new Comparator<T>() {
             @Override
             public int compare(T o1, T o2) {
@@ -73,31 +73,29 @@ public class LinkedList<T> implements Iterable<T> {
 
     @Override
     public Iterator<T> iterator() {
-	return null;
+        return null;
 
 	//ptr = head
-	//hasnext can i keep going is ptr == null
-	//next points to the next return value of T (T v =ptr.value;ptr=ptr.next;return v;
-	//remove don't need it
+        //hasnext can i keep going is ptr == null
+        //next points to the next return value of T (T v =ptr.value;ptr=ptr.next;return v;
+        //remove don't need it
     }
 
     static class MergeSort<T> { // object method pattern;
-     final Comparator<T> comp;
 
-     public MergeSort(Comparator<T> comp) {
-         this.comp = comp;
-     }
+        final Comparator<T> comp;
 
-     public void sort(LinkedList<T> list) {
+        public MergeSort(Comparator<T> comp) {
+            this.comp = comp;
+        }
 
+        public void sort(LinkedList<T> list) {
 
-     }
+        }
 
-     public void parallel_sort(LinkedList<T> list) {
+        public void parallel_sort(LinkedList<T> list) {
 
+        }
+    }
 
-     }
- }
-
- 
 }
