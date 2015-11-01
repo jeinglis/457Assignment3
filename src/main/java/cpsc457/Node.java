c011f951799d8ba7cc7490d4bf78e983ece5459a
James Inglis <jeinglis@ucalgary.ca>
2015-11-01 15:07:05 -0700

    Fixed my Node class

--- src/main/java/cpsc457/Node.java
+++ src/main/java/cpsc457/Node.java
@@ -1,18 +1,5 @@
 package cpsc457;
 
-import cpsc457.Node;
-
-public class Node<T extends Comparable> {
-
-    protected T itemM;
-    protected Node<T> nextM;
-
-    //default constructor
-    public Node()    {
-        itemM = null;
-        nextM = null;
-    }package cpsc457;
-
 public class Node<T extends Comparable> {
 
     protected T itemM;
@@ -23,58 +10,6 @@ public class Node<T extends Comparable> {
         itemM = null;
         nextM = null;
     }
-
-    public Node(T t) {
-        itemM = t;
-        nextM = null;
-    }
-    
-    //constructor given an item and next Node
-    public Node(T item, Node next) {
-        itemM = item;
-        nextM = next;
-
-    }
-
-    public boolean hasNext() {
-        if (nextM == null) {
-            return false;
-        }
-        return true;
-    }
-
-    public  void  setNext(Node<T> node) {
-        this.nextM = node;
-    }
-
-
-
-    public T getItem() {
-
-        return itemM;
-
-    }
-
-    public  void setItem(T item) {
-
-        itemM = item;
-
-    }
-
-    public  Node<T> getNext() {
-
-        return nextM;
-
-    }
-    
-    @Override
-    public String toString()    {
-        return itemM.toString();
-    }
-    
-
-}
-
 
     public Node(T t) {
         itemM = t;
@@ -124,6 +59,5 @@ public class Node<T extends Comparable> {
         return itemM.toString();
     }
     
-
 
 }
